9d1d564fae98df5cbb69edfaea34f520af9f78a5
KoooV <alexkovalev13681@gmail.com>
2025-07-07 21:51:37 +0300

    add 2-3

--- StreamAPI.java
+++ StreamAPI.java
@@ -9,4 +9,25 @@ public void merge(int[] nums1, int m, int[] nums2, int n) {
         //берем из res c 0 индекса, вставляем с 0 индекса в nums1
     }
       
+// leetcode https://leetcode.com/problems/sum-of-unique-elements
+public int sumOfUnique(int[] nums) {
+        return (int) IntStream.of(nums)
+            .filter(num -> Arrays.stream(nums)
+            .filter(x -> x == num).count() == 1)
+            .sum();
+    }
+    
+// leetcode https://leetcode.com/problems/single-number
+public int singleNumber(int[] nums) {
+        return Arrays.stream(nums).filter(num -> Arrays.stream(nums)
+            .filter(x -> x == num)
+            .count() == 1)
+            .findFirst()
+            .orElse(-1);
+    }
+
+
+
+
+
     
